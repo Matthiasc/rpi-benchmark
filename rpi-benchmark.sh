d1c02c36861d161ae15940ff2ce1f1f759dd4e22
Matthias <Matthias.crommelinck@gmail.com>
2025-10-02 15:20:09 +0200

    Refactor
    Updated speed test handling and improved script output. Changed sysbench command syntax and modified author information.

--- rpi-benchmark.sh
+++ rpi-benchmark.sh
@@ -9,16 +9,29 @@ fi
 if [ ! `which sysbench` ]; then
   apt-get install -y sysbench
 fi
-if [ ! `which speedtest-cli` ]; then
-  apt-get install -y speedtest-cli
-fi
+
+# Skip the speed test if the user doesn't want it to run
+case $1 in
+    --no-speedtest|-ns )
+        echo "Skipping speed test"
+        echo
+        ;;
+    * )
+        if [ ! `which speedtest-cli` ] && [ ! `which speedtest` ]; then
+          apt-get install -y speedtest-cli
+        fi
+        ;;
+esac
+
+# Get root disk for hdparam test
+ROOTDISK=`mount | grep " on / type" | cut -f 1 -d " "`
 
 # Script start!
 clear
 sync
 echo -e "\e[96mRaspberry Pi Benchmark Test"
-echo -e "Author: AikonCWD"
-echo -e "Version: 3.0\n\e[97m"
+echo -e "Original author: AikonCWD"
+echo -e "Version: 4.0\n\e[97m"
 
 # Show current hardware
 vcgencmd measure_temp
@@ -30,27 +43,49 @@ printf "sd_clock="
 grep "actual clock" /sys/kernel/debug/mmc0/ios 2>/dev/null | awk '{printf("%0.3f MHz", $3/1000000)}'
 echo -e "\n\e[93m"
 
-echo -e "Running InternetSpeed test...\e[94m"
-speedtest-cli --simple
+echo -e "System Memory Usage:\e[94m"
+free -h
 echo -e "\e[93m"
 
+echo -e "Disk Usage:\e[94m"
+df -h
+echo -e "\e[93m"
+
+
+# Identify the installed version of speedtest
+SPEEDTEST=""
+if [ `which speedtest` ]; then
+    SPEEDTEST="$(which speedtest) --simple"
+elif [ `which speedtest-cli` ]; then
+    SPEEDTEST="$(which speedtest-cli) --simple"
+else
+    echo -e "Failed to identify installed speedtest software\e[94m"
+fi
+if [ ! -z "$SPEEDTEST" ]; then
+    echo -e "Internet connection speed test will proceed with $SPEEDTEST\e[94m"
+        echo -e "Running InternetSpeed test...\e[94m"
+    eval $SPEEDTEST
+        echo -e "\e[93m"
+fi
+
+
 echo -e "Running CPU test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=cpu --cpu-max-prime=5000 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench cpu --threads=4 --validate=on --cpu-max-prime=5000 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
 echo -e "Running THREADS test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=threads --thread-yields=4000 --thread-locks=6 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench threads --threads=4 --validate=on --thread-yields=4000 --thread-locks=6 run | grep 'total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
 echo -e "Running MEMORY test...\e[94m"
-sysbench --num-threads=4 --validate=on --test=memory --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
+sysbench memory --threads=4 --validate=on --memory-block-size=1K --memory-total-size=3G --memory-access-mode=seq run | grep 'Operations\|transferred\|total time:\|min:\|avg:\|max:' | tr -s [:space:]
 vcgencmd measure_temp
 echo -e "\e[93m"
 
-echo -e "Running HDPARM test...\e[94m"
-hdparm -t /dev/mmcblk0 | grep Timing
+echo -e "Running HDPARM test on ${ROOTDISK}...\e[94m"
+hdparm -t ${ROOTDISK} | grep Timing
 vcgencmd measure_temp
 echo -e "\e[93m"
 
@@ -60,9 +95,9 @@ vcgencmd measure_temp
 echo -e "\e[93m"
 
 echo -e "Running DD READ test...\e[94m"
-echo -e 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
+echo 3 > /proc/sys/vm/drop_caches && sync && dd if=~/test.tmp of=/dev/null bs=1M 2>&1 | grep -v records
 vcgencmd measure_temp
 rm -f ~/test.tmp
 echo -e "\e[0m"
 
-echo -e "\e[91mAikonCWD's rpi-benchmark completed!\e[0m\n"
+echo -e "\e[91mrpi-benchmark completed!\e[0m\n"
